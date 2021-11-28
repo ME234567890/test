78aa8ddd710d75d01dbd88ae3001c96e958a1343
TEEJIO <42524279+ME234567890@users.noreply.github.com>
2021-11-28 17:55:30 -0500

    Create e.js

---
+++ e.js
@@ -0,0 +1,44 @@
+class Utilities {
+
+  constructor() {}
+
+  getInfo() {
+    return {
+      id: 'utilities',
+      name: 'Utlities',
+
+      color1: '#8BC34A',
+      color2: '#7CB342',
+      color3: '#689F38',
+
+      menuIconURI: icon,
+
+      blocks: [
+        {
+          opcode: 'img',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: 'draw image [img] at [x] [y]',
+          arguments: {
+            img: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'apple'
+            },
+            x: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 'apple'
+            },
+            y: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 'APPLE'
+            }
+          }
+        }
+          img({img, x, y}) {
+              var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  var img = document.getElementById("scream");
+  return ctx.drawImage(img, 90, 130, 50, 60, 10, 10, 50, 60);
+  }
+        Scratch.extensions.register(new Utilities());
