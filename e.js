fb458152788583f7859e2ad0bfd70c2c2b4a8b9e
TEEJIO <42524279+ME234567890@users.noreply.github.com>
2021-11-28 18:06:59 -0500

    Update e.js

--- e.js
+++ e.js
@@ -1,49 +1,61 @@
-class Utilities {
-
-  constructor() {}
-
-  getInfo() {
-    return {
-      id: 'utilities',
-      name: 'Utlities',
-
-      color1: '#8BC34A',
-      color2: '#7CB342',
-      color3: '#689F38',
-
-      menuIconURI: icon,
-
-      blocks: [
-        {
-          opcode: 'img',
-
-          blockType: Scratch.BlockType.REPORTER,
-
-          text: 'draw image [img] at [x] [y]',
-          arguments: {
-            img: {
-              type: Scratch.ArgumentType.STRING,
-              defaultValue: 'https://me234567890.github.io/test/gfd.png'
-            },
-            x: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '0'
-            },
-            y: {
-              type: Scratch.ArgumentType.NUMBER,
-              defaultValue: '0'
-            }
-          }
-        }
-      ]
+class ScratchFetch {
+    constructor() {
     }
-  }
+    
+    getInfo() {
+        return {
+            "id": "Fetch",
+            "name": "Fetch",
+            "blocks": [
+                        {
+                            "opcode": "fetchURL",
+                            "blockType": "reporter",
+                            "text": "fetch data from [url]",
+                            "arguments": {
+                                "url": {
+                                    "type": "string",
+                                    "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
+                                },
+                            }
+                        },
+                        {
+                            "opcode": "jsonExtract",
+                            "blockType": "reporter",
+                            "text": "extract [name] from [data]",
+                            "arguments": {
+                                "name": {
+                                    "type": "string",
+                                    "defaultValue": "temperature"
+                                },
+                                "data": {
+                                    "type": "string",
+                                    "defaultValue": '{"temperature": 12.3}'
+                                },
+                            }
+                        },
+                ],
+        };
+    }
+    
+    fetchURL({url}) {
+        return fetch(url).then(response => response.text())
+    }
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
+    }
 }
-          img({img, x, y}) {
-              var c = document.getElementById("myCanvas");
-              var ctx = c.getContext("2d");
-              var img = document.getElementById("scream");
-              return ctx.drawImage(img, 90, 130, 50, 60, 10, 10, 50, 60);
-  }
-  }
-        Scratch.extensions.register(new Utilities());
+
+Scratch.extensions.register(new ScratchFetch())
